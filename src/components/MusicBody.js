d2c4ff0b2d597057d629c2bbac7656e0efec4ed2
cjmcd123 <cj_mcdowall@btinternet.com>
2018-08-15 21:28:26 +0100

    loading creen & stopping multiple audio elements playing

--- src/components/MusicBody.js
+++ src/components/MusicBody.js
@@ -3,13 +3,13 @@ import React from "react";
 const MusicBody = (props) => {
   const body = props.music.map((musicItem, index) => {
     return <tr key={index} className={index}>
-      <td>{index + 1}</td>
+      <td>{index + 1}.</td>
       <td><img src={musicItem["im:image"][2].label} alt="Album Cover Not Available"/></td>
       <td>{musicItem["im:name"].label}</td>
       <td>{musicItem["im:artist"].label}</td>
       <td>
-        <audio controls>
-          <source src={musicItem.link[1].attributes.href} type="audio/x-m4a"/>
+        <audio controls controlsList="nodownload">
+          <source src={musicItem.link[1].attributes.href} type={musicItem.link[1].attributes.type}/>
           Your browser does not support the audio element.
         </audio>
       </td>
--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -14,9 +14,22 @@ class MusicBox extends React.Component {
     fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/json")
     .then(responseText => responseText.json())
     .then(musicData => this.setState({music: musicData.feed.entry}));
+
+    document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] !== e.target){
+            audios[i].pause();
+        }
+      }
+    }, true);
   }
 
+
   render(){
+    if (this.state.music == null || this.state.music.length === 0) {
+      return <p>Loading...</p>;
+    }
     return (
       <div className="music-box">
         <h1>G6 UK Top 20 Songs</h1>
